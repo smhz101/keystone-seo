1bf2ee0daa8ac83f03fb1b245c1b5a33f7ddafaa
Muzammil Hussain <smhz101@gmail.com>
2025-08-16 18:52:52 +0500

    (added) path wrappers

--- src/Support/Paths.php
+++ src/Support/Paths.php
@@ -9,4 +9,8 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  * @since 0.1.0
  */
 class Paths {
+	public function base_dir() { return KEYSTONE_SEO_DIR; }
+	public function base_url() { return KEYSTONE_SEO_URL; }
+	public function views_dir() { return KEYSTONE_SEO_DIR . 'views/'; }
+	public function assets_url() { return KEYSTONE_SEO_URL . 'assets/'; }
 }
