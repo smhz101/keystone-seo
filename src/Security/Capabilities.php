120b56ab51b0a3409904f25fe3ad5ed0d414d799
Muzammil Hussain <smhz101@gmail.com>
2025-08-16 18:54:35 +0500

    (added) capability check wrapper

--- src/Security/Capabilities.php
+++ src/Security/Capabilities.php
@@ -9,4 +9,21 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  * @since 0.1.0
  */
 class Capabilities {
+	/**
+	 * Returns the capability required for managing settings.
+	 *
+	 * @return string
+	 */
+	public function manage_settings_cap() {
+		return 'manage_options';
+	}
+
+	/**
+	 * Check if current user can manage Keystone settings.
+	 *
+	 * @return bool
+	 */
+	public function can_manage_settings() {
+		return current_user_can( $this->manage_settings_cap() );
+	}
 }
