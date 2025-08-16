aacceec2963471ab95c2a1c72f1f353b1c232f7c
Muzammil Hussain <smhz101@gmail.com>
2025-08-16 19:20:05 +0500

    (added) WP-CLI command to render sitemap index

--- src/Cli/Commands.php
+++ src/Cli/Commands.php
@@ -1,6 +1,8 @@
 <?php
 namespace Keystone\Cli;
 
+use Keystone\Sitemap\SitemapProvider;
+
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 /**
@@ -9,4 +11,34 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  * @since 0.1.0
  */
 class Commands {
+	/** @var SitemapProvider */
+	protected $provider;
+
+	public function __construct( SitemapProvider $provider ) {
+		$this->provider = $provider;
+	}
+
+	/**
+	 * Register CLI commands.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		if ( ! class_exists( '\WP_CLI' ) ) {
+			return;
+		}
+		\WP_CLI::add_command( 'keystone sitemap:render-index', array( $this, 'cmd_render_index' ) );
+	}
+
+	/**
+	 * Output sitemap index XML to STDOUT (for debug).
+	 *
+	 * ## EXAMPLES
+	 *     wp keystone sitemap:render-index
+	 *
+	 * @return void
+	 */
+	public function cmd_render_index() {
+		\WP_CLI::line( $this->provider->render_index() );
+	}
 }
