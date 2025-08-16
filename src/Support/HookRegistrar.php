d17d9013d808b69138c785e487f183861e123064
Muzammil Hussain <smhz101@gmail.com>
2025-08-16 18:51:47 +0500

    (added) Centralized hook registration to avoid anonymous closures in hooks/filters

--- src/Support/HookRegistrar.php
+++ src/Support/HookRegistrar.php
@@ -9,4 +9,31 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  * @since 0.1.0
  */
 class HookRegistrar {
+  /**
+	 * Register a WordPress action.
+	 *
+	 * @param string $hook Hook name.
+	 * @param object $object Instance.
+	 * @param string $method Method name.
+	 * @param int    $priority Priority.
+	 * @param int    $args Accepted args.
+	 * @return void
+	 */
+	public function action( $hook, $object, $method, $priority = 10, $args = 1 ) {
+		add_action( $hook, array( $object, $method ), $priority, $args );
+	}
+
+	/**
+	 * Register a WordPress filter.
+	 *
+	 * @param string $hook Hook name.
+	 * @param object $object Instance.
+	 * @param string $method Method name.
+	 * @param int    $priority Priority.
+	 * @param int    $args Accepted args.
+	 * @return void
+	 */
+	public function filter( $hook, $object, $method, $priority = 10, $args = 1 ) {
+		add_filter( $hook, array( $object, $method ), $priority, $args );
+	}
 }
