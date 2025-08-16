e58101666459423cac36d09e56e8199fdb031fb1
Muzammil Hussain <smhz101@gmail.com>
2025-08-16 18:58:44 +0500

    (added) Main Menu

--- src/Admin/AdminMenu.php
+++ src/Admin/AdminMenu.php
@@ -1,6 +1,8 @@
 <?php
 namespace Keystone\Admin;
 
+use Keystone\Security\Capabilities;
+
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 /**
@@ -9,4 +11,27 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  * @since 0.1.0
  */
 class AdminMenu {
+  /** @var Capabilities */
+	protected $caps;
+
+  public function __construct( Capabilities $caps ) {
+		$this->caps = $caps;
+	}
+
+	/**
+	 * Add menu pages.
+	 *
+	 * @return void
+	 */
+	public function register_menu() {
+		add_menu_page(
+			'Keystone SEO',
+			'Keystone SEO',
+			$this->caps->manage_settings_cap(),
+			'keystone-seo',
+			'render_page',
+			'dashicons-shield-alt',
+			58
+		);
+	}
 }
