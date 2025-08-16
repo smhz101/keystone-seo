f0538647a6831e5fe603844a080bda25243f3581
Muzammil Hussain <smhz101@gmail.com>
2025-08-16 18:53:36 +0500

    (added) logger wrapper

--- src/Support/Logger.php
+++ src/Support/Logger.php
@@ -9,4 +9,20 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  * @since 0.1.0
  */
 class Logger {
+	/**
+	 * Log a message to error_log with context.
+	 *
+	 * @param string $level debug|info|warning|error.
+	 * @param string $message Message.
+	 * @param array  $context Extra data.
+	 * @return void
+	 */
+	public function log( $level, $message, $context = array() ) {
+		$prefix = '[Keystone SEO][' . strtoupper( $level ) . '] ';
+		$line   = $prefix . $message;
+		if ( ! empty( $context ) ) {
+			$line .= ' ' . wp_json_encode( $context );
+		}
+		error_log( $line ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+	}
 }
